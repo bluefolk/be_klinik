d394917b60de894a411fe9fcac7b8d61e5cfb8ba
Ferantipramesty <ferantidestina@gmail.com>
2025-08-05 17:23:00 +0700

    Modify server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/api/notification/midtrans', async (req, res) => {
 
 // Forward to transaction handler
 const response = await fetch(`https://be-klinik.onrender.com/api/notification`, {
-// const response = await fetch(`http://172.20.10.3:3001/api/notification`, {
+//const response = await fetch(`http://172.20.10.3:3001/api/notification`, {
 
 
       method: 'POST',
